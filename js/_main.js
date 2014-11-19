e4ea62b97ddb5bc678c142698d861287f41501a0
lxMBIT <lxmbit@gmail.com>
2014-11-19 07:14:17 +0200

    1

--- js/_main.js
+++ js/_main.js
@@ -1,24 +1,45 @@
 // Off Canvas Sliding
 
-$(document).ready(function(){
-	$('.js-menu-trigger').on('click touchstart', function(e){
-		$('body').toggleClass('no-scroll');
-		$('.js-menu, .js-menu-screen').toggleClass('is-visible');
-		$('.sliding-menu-button').toggleClass('slide close');
-		$('#masthead, #page-wrapper').toggleClass('slide');
-		e.preventDefault();
-	});
-	$('.js-menu-screen').on('click touchstart', function(e){
-		$('body').toggleClass('no-scroll');
-		$('.js-menu, .js-menu-screen').toggleClass('is-visible');
-		$('.sliding-menu-button').toggleClass('slide close');
-		$('#masthead, #page-wrapper').toggleClass('slide');
-		e.preventDefault();
-	});
+$(document).ready(function() {
+    $('.js-menu-trigger').on('click touchstart', function(e) {
+        $('body').toggleClass('no-scroll');
+        $('.js-menu, .js-menu-screen').toggleClass('is-visible');
+        $('.sliding-menu-button').toggleClass('slide close');
+        $('#masthead, #page-wrapper').toggleClass('slide');
+        e.preventDefault();
+    });
+    $('.js-menu-screen').on('click touchstart', function(e) {
+        $('body').toggleClass('no-scroll');
+        $('.js-menu, .js-menu-screen').toggleClass('is-visible');
+        $('.sliding-menu-button').toggleClass('slide close');
+        $('#masthead, #page-wrapper').toggleClass('slide');
+        e.preventDefault();
+    });
 });
 
 // FitVids
-$(document).ready(function(){
-	// Target your .container, .wrapper, .post, etc.
-	$("#main").fitVids();
+$(document).ready(function() {
+    // Target your .container, .wrapper, .post, etc.
+    $("#main").fitVids();
+});
+
+//SLIDER
+$(document).ready(function() {
+
+    $("#owl-demo").owlCarousel({
+
+        navigation: false, // Show next and prev buttons
+        slideSpeed: 300,
+        paginationSpeed: 400,
+        singleItem: true
+
+        // "singleItem:true" is a shortcut for:
+        // items : 1, 
+        // itemsDesktop : false,
+        // itemsDesktopSmall : false,
+        // itemsTablet: false,
+        // itemsMobile : false
+
+    });
+
 });
