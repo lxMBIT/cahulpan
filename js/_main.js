c3ba404b5ab64eeaea4cff495c4da8ff89c0b75f
lxMBIT <lxmbit@gmail.com>
2014-11-19 07:22:36 +0200

    1

--- js/_main.js
+++ js/_main.js
@@ -24,22 +24,3 @@ $(document).ready(function() {
 });
 
 //SLIDER
-$(document).ready(function() {
-
-    $("#owl-example").owlCarousel({
-
-        navigation: false, // Show next and prev buttons
-        slideSpeed: 300,
-        paginationSpeed: 400,
-        singleItem: true
-
-        // "singleItem:true" is a shortcut for:
-        // items : 1, 
-        // itemsDesktop : false,
-        // itemsDesktopSmall : false,
-        // itemsTablet: false,
-        // itemsMobile : false
-
-    });
-
-});
